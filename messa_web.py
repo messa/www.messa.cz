e2a570901c3cab9cbf2dd82e54f6831e9db1b53f
Petr Messner <petr.messner@gmail.com>
2017-01-01 02:03:53 +0100

    add some comments

--- messa_web.py
+++ messa_web.py
@@ -10,6 +10,11 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
+    '''
+    Redirect to /en/ or /cs/ based on reported browser language preferences.
+    '''
+    # This is too simle, but I believe it's OK.
+    # Of course more perfect solution would be to parse the header.
     al = request.headers.get('Accept-Language', '')
     if 'cs' in al.lower():
         return flask.redirect('/cs/')
@@ -18,6 +23,8 @@ def index():
 
 @app.route('/en/')
 def index_en():
+    # Both language versions are stored in a single template, differentiated
+    # only using the variables 'en' and 'cs' (True/False).
     return render_template('index.html', en=True)
 
 
@@ -27,7 +34,11 @@ def index_cs():
 
 
 @app.route('/<string:filename>')
-def favicon(filename):
+def favicon_file(filename):
+    '''
+    All the favicon-related stuff is expected to be served from web root.
+    This route serves it (from real directory static/favicon).
+    '''
     return flask.send_from_directory(
         str(Path(app.root_path) / 'static/favicon'),
         filename)
