48bb4329a5b3c503435eba8fcaab1b7a3ea95fea
Petr Messner <petr.messner@gmail.com>
2019-08-05 13:44:45 +0200

    add CSS grid

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link, graphql, useStaticQuery } from "gatsby"
 
+import './Layout.css'
+
 const staticQuery = graphql`
   query LayoutQuery {
     site {
@@ -15,19 +17,23 @@ function Layout({ children }) {
   const data = useStaticQuery(staticQuery)
   const { siteMetadata } = data.site
   return (
-    <div>
-      <h1 className='siteTitle'>{siteMetadata.title}</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>home</Link>
-          </li>
-          <li>
-            <Link to='/about'>about</Link>
-          </li>
-        </ul>
-      </nav>
-      {children}
+    <div className='Layout'>
+      <div className='navigation'>
+        <h1 className='siteTitle'>{siteMetadata.title}</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>home</Link>
+            </li>
+            <li>
+              <Link to='/about'>about</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className='mainContent'>
+        {children}
+      </div>
     </div>
   )
 }
