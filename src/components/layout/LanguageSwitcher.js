36cc45cea9306073b4d1a8f1bdb82c34ee7ce311
Petr Messner <petr.messner@gmail.com>
2019-08-11 22:35:20 +0200

    CSS tweaks

--- src/components/layout/LanguageSwitcher.js
+++ src/components/layout/LanguageSwitcher.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { EN, CS } from '../l10n'
 
+import './LanguageSwitcher.css'
+
 function LanguageSwitcher({ enLink, csLink }) {
   return (
     <div className='LanguageSwitcher'>
-      <EN><span className='active'>english</span></EN>
+      <EN><b>english</b></EN>
       <CS><Link to={enLink || '/en/'}>english</Link></CS>
       {' · '}
-      <CS><span className='active'>česky</span></CS>
+      <CS><b>česky</b></CS>
       <EN><Link to={csLink || '/cs/'}>česky</Link></EN>
     </div>
   )
